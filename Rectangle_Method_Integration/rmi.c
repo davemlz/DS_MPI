12df9857e791597be6981299e5e767ccb56ff016
David Montero <49817852+davemlz@users.noreply.github.com>
2019-04-20 14:17:44 -0500

    Add files via upload

---
+++ Rectangle_Method_Integration/rmi.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+main(int argc, char* argv)
+{
+	// Definiciones
+	double a = 0, b = 1, w, sum = 0, x_i;
+	int i, n;
+	
+	// Cantidad de rectangulos
+	n = 100000;
+	
+	// Ancho de rectangulos
+	w = (b-a)/n;	
+
+	// Inicio de algoritmo del rectangulo
+	for(i = 1;i <= n;i++){
+		
+		// Valor que toma x_i
+		x_i = a + (i-1)*w;
+
+		// Calculo de la sumatoria del rectangulo: ancho x largo
+		// Funcion: x^3
+		sum = sum + ( w * (x_i * x_i * x_i) );
+		
+	}
+	
+	// Imprimir en pantalla
+	printf("Valor aproximado de la integral desde 0 a 1 de x elevado a la 3 = %d\n",sum);
+	
+	// Finalizar
+	return 0;
+}
